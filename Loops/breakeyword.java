99b233af70ae646257244eb1efee5536cbc6d49a
manhuu14 <umanhuu@gmail.com>
2025-05-23 08:16:57 +0530

    added loops

---
+++ Loops/breakeyword.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class breakeyword {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        do { 
+            System.out.println("enter you number");
+            int n=sc.nextInt();  
+            if(n%10==0){
+                break;
+            } 
+            System.out.println(n);
+        } while(true);
+    }
+}
